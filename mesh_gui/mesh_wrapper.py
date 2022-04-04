12465112c374e0a20a60032ef1ae6a2ae3b1e39b
Matthew O'Neill <oneill_91@hotmail.co.uk>
2022-04-04 22:10:07 +0100

    add simple gui that allows a user to input fields

--- mesh_gui/mesh_wrapper.py
+++ mesh_gui/mesh_wrapper.py
@@ -41,3 +41,65 @@ class MeshSender:
             with open(os.path.join(dest_folder, f"{file}.ctl"), "w") as f:
                 f.write(self.generate_ctl())
             return filename
+
+
+def event_loop(layout):
+    window = sg.Window(title="outbox", layout=layout, resizable=True)
+
+    vals = {}
+    while True:
+        event, values = window.read()
+        window["error_text"].update("")
+        vals.update(values)
+        match event:
+            case sg.WIN_CLOSED:
+                break
+            case "browse":
+                file_names = sg.filedialog.askopenfilenames()
+                window["filenames"].update(file_names)
+            case "send":
+                try:
+                    MeshSender(**vals).send_file("test_folders/mailbox/out")
+                except:
+                    print(window.element_list())
+                    window["error_text"].update("Missing fields marked with an *")
+            case _:
+                pass
+        print(event, vals)
+    window.close()
+
+
+def main():
+    layout = [
+        [sg.Text("MESH Outbox")],
+        [sg.Text("From:*\t\t"), sg.Input(key="sender", tooltip="Your Mailbox ID")],
+        [
+            sg.Text("To:*\t\t"),
+            sg.Input(key="recipient", tooltip="Recipients Mailbox ID"),
+        ],
+        [
+            sg.Text("WorkflowID:*\t"),
+            sg.Input(
+                key="workflowID", tooltip="The workflow ID for the data collection"
+            ),
+        ],
+        [sg.Text("Subject:\t\t"), sg.Input(key="subject", tooltip="Subject")],
+        [sg.Text("LocalID:\t\t"), sg.Input(key="localID")],
+        [
+            sg.Text("Data file(s)*\t"),
+            sg.Button(
+                "Browse",
+                tooltip="hold shift and click to select multiple files",
+                key="browse",
+            ),
+        ],
+        [sg.Listbox(values=[], size=(60, 4), key="filenames")],
+        [sg.HorizontalSeparator()],
+        [sg.Button("Send", key="send")],
+        [sg.Text(text_color="red", key="error_text")],
+    ]
+    event_loop(layout=layout)
+
+
+if __name__ == "__main__":
+    main()
