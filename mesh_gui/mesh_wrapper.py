23e598988cabb6e781e3f2cf45273a105f997c20
Matthew O'Neill <oneill_91@hotmail.co.uk>
2022-04-06 21:03:05 +0100

    add multiple boxes to mesh_wrapper
    allow programmatic generation of mailboxes

--- mesh_gui/mesh_wrapper.py
+++ mesh_gui/mesh_wrapper.py
@@ -1,7 +1,4 @@
-import shutil
-import os
 import PySimpleGUI as sg
-import xml.etree.ElementTree as ET
 import tabs.send_files as send_files
 import tabs.inbox as inbox
 
@@ -9,25 +6,43 @@ import tabs.inbox as inbox
 def event_loop(layout):
     window = sg.Window(title="MESH", layout=layout, resizable=True)
     file_sender = send_files.Send_Files()
-    inbox_ = inbox.Inbox()
-    tabs = [file_sender, inbox_]
+    boxes = dict(
+        INBOX_TAB=inbox.Mesh_box("inbox", "test_folders/mailbox/in"),
+        OUTBOX_TAB=inbox.Mesh_box("outbox", "test_folders/mailbox/out"),
+        SENT_TAB=inbox.Mesh_box("sent", "test_folders/mailbox/sent"),
+    )
     while True:
         event, values = window.read(timeout=10000)
+        for box in boxes.values():
+            box.update_inbox_tab(window)
         window["error_text"].update("")
         match event, values:
             case sg.WIN_CLOSED, _:
                 break
 
-            case _, {"tabs": "Send Message"}:
+            case _, {"-tabs-": "Send Message"}:
                 file_sender.loop(event=event, values=values, window=window)
 
-            case _, {"tabs": "Inbox"}:
-                inbox_.loop(event, values, window)
+            case _, {"-tabs-": tabname}:
+                boxes[tabname].loop(event, values, window)
+
             case "__TIMEOUT__", _:
-                for tab in tabs:
-                    tab.loop(event, values, window)
-        print(event)
+                for box in boxes.values():
+                    box.loop(event, values, window)
+
+        print(event, values)
     window.close()
+
+
+def generate_boxes():
+    for name in ["Inbox", "Outbox", "Sent"]:
+        yield sg.Tab(
+            name,
+            inbox.generate_box_layout(name.lower()),
+            key=f"{name.upper()}_TAB",
+            expand_x=True,
+            expand_y=True,
+        )
 
 
 def main():
@@ -37,13 +52,23 @@ def main():
             sg.TabGroup(
                 [
                     [
-                        sg.Tab("Send Message", send_files.SEND_MESSAGE_LAYOUT),
-                        sg.Tab("Inbox", inbox.INBOX_LAYOUT),
+                        sg.Tab(
+                            "Send Message",
+                            send_files.SEND_MESSAGE_LAYOUT,
+                            expand_x=True,
+                            expand_y=True,
+                        ),
+                        *generate_boxes(),
                     ]
                 ],
-                key="tabs",
-            )
-        ]
+                key="-tabs-",
+                enable_events=True,
+                expand_x=True,
+                expand_y=True,
+                tab_location=sg.TAB_LOCATION_TOP_LEFT,
+                size=(1000, 700),
+            ),
+        ],
     ]
     event_loop(layout=layout)
 
